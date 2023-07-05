6d37f722174043c8d8514b305de2d1e1f00d04c8
BookmDan <danoh329@gmail.com>
2023-07-04 22:10:33 -0700

    udpate for loop

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+function writeCards(stringNames, eventName) {
+  // const array = ['apple', 'banana', 'orange', 'grape'];
+  // const array = []
+  for (let counter = 0; counter < stringNames.length; counter++) {
+    // console.log('Counter:', counter);
+    console.log(`Thank you, ${stringNames[counter]}, for the wonderful ${eventName} gift!`);
+
+  }
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
