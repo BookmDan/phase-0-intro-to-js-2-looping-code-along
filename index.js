8e5d9896210940047aa2c14285829b605c348050
BookmDan <danoh329@gmail.com>
2023-07-04 22:16:21 -0700

    test test test

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 // Code your solutions in this file
 function writeCards(stringNames, eventName) {
   // const array = ['apple', 'banana', 'orange', 'grape'];
-  // const array = []
+  const array = []
   for (let counter = 0; counter < stringNames.length; counter++) {
     // console.log('Counter:', counter);
-    console.log(`Thank you, ${stringNames[counter]}, for the wonderful ${eventName} gift!`);
+    array.push(
+    `Thank you, ${stringNames[counter]}, for the wonderful ${eventName} gift!`);
 
   }
+  return array;
 }
 
 writeCards(["Charlie", "Samip", "Ali"], "birthday");
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+countDown(10);
